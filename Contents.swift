968a2a0e8483c2697ba2e775429a383a277bd5c5
kiranmehal <35242853+kiranmehal@users.noreply.github.com>
2018-01-30 20:44:46 -0500

    [sets][dictionary][arrays]

--- Contents.swift
+++ Contents.swift
@@ -3,112 +3,39 @@
 import UIKit
 
 var str = "Hello, playground"
-var s:Int
-s=100
-var a,b,c:Int
-a=1
-b=2
-c=3
-print(a)
-let x=1000
-let y:Int
-y=10000
+var letters=Set<Character>()
+letters.insert("a")
+letters.insert("b")
+letters.insert("a")
 
-var z=y
-z=10
+print(letters.count)
 
-print(str)
-print(s)
-print(x,y)
-c=a+b
-print(c)
-print(a,"+",b,"=",c,separator:"###",terminator:"---")
-print("\(a)+\(b)=\(c)")
-if a>b
-{
-    if a>c
-    {
-    print("a is greater")
-    }
-    else
-    {
-        print("c is greater")
-    }
-}
-else
-{
-    if b>c
-    {print("b is greater")}
-else
-{   print("c is greater")
-}
-}
 
-//logical operator
-if a>b&&a>c
-{
-    print("a is max")
+var strr="hello world😍"
+
+var a="\u{24}"
+var b="\u{1f496}"
+print(a,b)
+var c="\u{65}"
+var d="\u{1F467}"
+print(d)
+let longstring="""
+bjhgjhgu jhgju
+dfgr
+rththtyh
+"""
+print(longstring)
+
+print("\"hello\",kiran")
+
+var s=String()
+
+s="welcome to lambton"
+
+for c in s{
     
-}
-else
-{
-    if b>c
-    {
-        print("b is max")
-        
-    }
-else
-    {
-        print("c is max")
-        
-}
+    print(c)
 }
 
-//logical and
-
-if a>b&&a>c
-{
-    print("a is max")
-    
-}
-else if b>c
-{
-    print("b is max")
-}
-else
-{
-    print("c is max")
-}
-
-
-var i,n:Int
-i=1
-n=10
-for i in 1...n-1{
-    print(i)
-}
-
-
-for i in 1...10{
-    print("\(i * 5)")
-}
-
-
-for i in stride(from:0,to:50,by:5){
-    print(i)
-}
-
-
-
-
-var j=0
-repeat
-{
-    print(j)
-    j=j+1
-}
-while(j < 10)
-
-
-
+let s1="kirandeep"
 
