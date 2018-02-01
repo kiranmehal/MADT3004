bb950543b07767449e46720d17cb895d66aea545
kiranmehal <35242853+kiranmehal@users.noreply.github.com>
2018-01-31 20:10:29 -0500

    [strings][arrays operations][functions][default function]

--- Contents.swift
+++ Contents.swift
@@ -3,39 +3,173 @@
 import UIKit
 
 var str = "Hello, playground"
-var letters=Set<Character>()
-letters.insert("a")
-letters.insert("b")
-letters.insert("a")
+func display()
+{
+    print("kirandeep")
+}
+display()
 
-print(letters.count)
+func display(name:String)
+{
+    print("welcome,\(name)")
+}
+display(name:"kirandeep")
+
+//use of parametr in call of function
+func display(n:Int)
+{
+    for i in 1...n
+    {
+        print(i)
+    }
+}
+display(n:5) //here n: is must we can not omit this
+
+//use of (_) to omit use of parameter while displaying
+func display(_ n:Int) // n necomes optional
+{
+    for i in 1...n
+    {
+        print(i)
+    }
+}
+display(5)
+
+//funch with 1 parameter
+func display(number n:Int)
+{
+    for i in 1...n
+    {
+        print(i)
+    }
+}
+display(number:5)
+
+//funch with 2 parameters
+func sum(a:Int,b:Int)
+{
+print("add : \(a+b)")
+}
+sum(a:10,b:20)
 
 
-var strr="hello world😍"
+//eng form using lables
+func sum(of a:Int,and b:Int)
+{
+    print("add \(a+b)")
+}
+sum(of:10,and:30)
 
-var a="\u{24}"
-var b="\u{1f496}"
-print(a,b)
-var c="\u{65}"
-var d="\u{1F467}"
-print(d)
-let longstring="""
-bjhgjhgu jhgju
-dfgr
-rththtyh
-"""
-print(longstring)
+func greet()->String
+{
+return "welcome to toronto"
+}
+//greet()
+var s=greet()
+print(s)
+print(greet())
 
-print("\"hello\",kiran")
+//interger type
+/*func add(c:Int,d:Int)
+{
+    return "addition ,\(c+d)+"
+}
+//float type
+func add(c:Float,d:Float)
+{
+    return "addtion of float,\(c+d)"
+}
+//double
+func add(c:Double,d:Double)
+{
+    return "\(c+d)"
+}*/
 
-var s=String()
-
-s="welcome to lambton"
-
-for c in s{
-    
-    print(c)
+func add(_ a:Int,_ b:Int)->Int
+{
+    return a+b
 }
 
-let s1="kirandeep"
+func add(_ a:String,_ b:String)->String
+{
+    return a+b
+}
 
+print(add(3,4))
+print(add("hello","world"))
+
+//swipee strings
+func swip(a:String ,b:String)->(String,String)
+{
+    return(b,a)
+}
+let x=swip(a: "kiran",b: "mehal")
+print(x.0,x.1)
+
+//swipe numbers
+func swip(a: Int,b:Int)->(a:Int,b:Int)
+{
+    return (b,a)
+}
+let z=swip(a:100,b:200)
+print(z.a,z.b)
+
+
+//array elements sum
+func addarry(a: [Int])->Int
+{ var sum=0
+    for k in a
+    {
+      sum=sum+k
+        
+    }
+    return sum
+    
+}
+print(addarry(a: [1,2,3]))
+
+var na=[100,200,50,140]
+print(addarry(a: na))
+//var myMax = sort(myArray,>)[0]
+//find min maxcin array
+func findminmax(arr:[Int])->(min:Int,max:Int)
+{
+    return (arr.min() ?? 0,arr.max() ?? 0)
+}
+var minmax=findminmax(arr: [23,45,37,89,90,12])
+print(minmax.min,minmax.max)
+
+//find maximum
+var myArray:[Int]=[2,45,67,34,89,190]
+var myMax = 0
+for i in 0..<myArray.count {
+    if (myArray[i] > myMax){myMax = myArray[i]}
+}
+print("maximum is \(myMax)")
+
+//find minimum
+var array:[Int]=[200,45,67,34,89,190]
+var myMin=array[0]
+for i in 0..<array.count {
+    if (array[i] < myMin){myMin = array[i]}
+}
+print("minimum is \(myMin)")
+
+
+//default function value
+func area( l:Float,  b:Float=6.5)->(Float)
+{
+    return (l * b)
+}
+print(area(l:5.0))
+
+
+func area(_ l:Float,_ b:Float,_ h:Float=2.0)->Float
+{
+  return(l*b*h)
+}
+print(area(3.0,4.0))
+
+
+//iout example
+func swaptwoints(_ a: inout Int,_ b: inout )
